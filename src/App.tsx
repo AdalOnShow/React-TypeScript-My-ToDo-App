2ba3bfc02d443f37813ffc0a3e6b66f760869d4f
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-12-31 08:20:54 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <main>
+      <h2 className="text-orange-400 font-bold text-4xl text-center">Hello TypeScript To-Do App</h2>
+    </main>
+  )
+}
+export default App
