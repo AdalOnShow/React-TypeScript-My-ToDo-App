4d9a232da052b680be4d5d639263e6e3051ad2d2
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-03 11:17:56 +0600

    add handler for deleting todos in App component

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,10 @@ const App = () => {
     setEditingContent("")
   }
 
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
   return (
     <main className="h-screen flex justify-center items-center bg-[url(/bg.svg)]">
       <div className="w-10/12 max-w-md md:max-w-2xl mx-auto bg-transparent border-4 border-green-600 rounded-2xl backdrop-blur-sm px-8 md:px-20 py-8 md:py-20">
