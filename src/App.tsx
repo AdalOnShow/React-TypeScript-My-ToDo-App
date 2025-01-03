2ddc598478046e3d01baba3a05e45c7fc2e32902
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-03 11:18:35 +0600

    add handler for canceling todo edits in App component

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,11 @@ const App = () => {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  cosnt handleEditCancel = () => {
+    setEditingId(null)
+    setEditingContent("")
+  }
+
   return (
     <main className="h-screen flex justify-center items-center bg-[url(/bg.svg)]">
       <div className="w-10/12 max-w-md md:max-w-2xl mx-auto bg-transparent border-4 border-green-600 rounded-2xl backdrop-blur-sm px-8 md:px-20 py-8 md:py-20">
