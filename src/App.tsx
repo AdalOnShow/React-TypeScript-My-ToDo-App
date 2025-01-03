ce211dc091bbb7fbd310402f506e199b87887ca8
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-03 11:09:57 +0600

    add background SVG and update App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 const App = () => {
   return (
-    <main>
-      <h2 className="text-orange-400 font-bold text-4xl text-center">Hello TypeScript To-Do App</h2>
+    <main className="h-screen flex justify-center items-center bg-[url(/bg.svg)]">
+      <div className="w-10/12 max-w-md md:max-w-2xl mx-auto bg-transparent border-4 border-green-600 rounded-2xl backdrop-blur-sm px-8 md:px-20 py-8 md:py-20">
+        <div className="">
+          <h1 className="text-2xl md:text-4xl font-bold text-center text-green-600">TypeScript To-Do App</h1>
+        </div>
+      </div>
     </main>
   )
 }
+
 export default App
